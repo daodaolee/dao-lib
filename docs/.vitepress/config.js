7d55cf54276633ba6e2fdf011430015056dbba01
daodaolee <im@daodaolee.cn>
2022-10-06 00:15:37 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+  themeConfig: {
+    siteTitle: false,
+    logo: "/logo.png",
+    nav: [
+      {
+        text: "剪辑",
+        items: [
+          { text: "图片颜色识别", link: "/clip/index" }
+        ],
+      },
+      { text: "游戏", link: "/game" },
+      { text: "Github", link: "https://github.com/daodaolee" },
+    ],
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+title: Docs with VitePress
+editLink: true
+---
+
+# {{ $frontmatter.title }}
+
+```js{2}
+export default {
+  name: 'MyComponent',
+  // ...
+}
+```
+
+
